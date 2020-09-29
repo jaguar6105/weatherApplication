e7c76e79af66b028375d7feb300033dc09a09bd8
jaguar6105 <gilljoseph603@gmail.com>
2020-09-29 13:15:31 -0400

    update display with search data

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-
+var display = {
+    city: "",
+    temp: "",
+    humid: "",
+    wind: "",
+    uv: ""
+};
 
 
 var apiKey = "9079e0330fe858fe04fbf89c11cb7f8c";
 
 
 $(".citySearch").on("click", function() {
-    var city = $(".cityInput").val();
-    console.log(city)
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=9079e0330fe858fe04fbf89c11cb7f8c" //+ apiKey;
+    var citySearch = $(".cityInput").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+citySearch+"&appid=9079e0330fe858fe04fbf89c11cb7f8c" //+ apiKey;
+
+    display.city = citySearch;
+    updateDisplay();
+
 
     $.ajax({
         url: queryURL,
@@ -15,5 +24,18 @@ $(".citySearch").on("click", function() {
       })
         .then(function(response) {
           console.log(response);
+          display.city = citySearch;
+          updateDisplay();
         });
     });
+
+    function updateDisplay() {
+
+        $(".city").text(display.city);
+        $(".temp").text("Temperature: " + display.temp + " F");
+        $(".humid").text("Humidity: " + display.humid + "%");
+        $(".wind").text("Wind Speed: " + display.wind + " MPH");
+        $(".uv").text("UV Index: " + display.uv);
+
+
+    }
