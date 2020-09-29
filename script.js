819819a5913c300a35ad6ccc436e1b3bc0c6beb9
jaguar6105 <gilljoseph603@gmail.com>
2020-09-29 13:52:11 -0400

    add dynamic list of searches

--- script.js
+++ script.js
@@ -6,29 +6,17 @@ var display = {
     uv: ""
 };
 
-
-var apiKey = "9079e0330fe858fe04fbf89c11cb7f8c";
+var cityArray = [];
 
 
 $(".citySearch").on("click", function() {
     var citySearch = $(".cityInput").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+citySearch+"&appid=9079e0330fe858fe04fbf89c11cb7f8c" //+ apiKey;
 
-    display.city = citySearch;
-    updateDisplay();
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        .then(function(response) {
-          console.log(response);
-          display.city = citySearch;
-          updateDisplay();
-        });
+    searchDiv(citySearch);
+    apiSearch(citySearch);
     });
 
+    //display text in jumbotron
     function updateDisplay() {
 
         $(".city").text(display.city);
@@ -36,6 +24,41 @@ $(".citySearch").on("click", function() {
         $(".humid").text("Humidity: " + display.humid + "%");
         $(".wind").text("Wind Speed: " + display.wind + " MPH");
         $(".uv").text("UV Index: " + display.uv);
+    }
+
+    function apiSearch(city) {
+        var citySearch = city;
+        var apiKey = "9079e0330fe858fe04fbf89c11cb7f8c";
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+citySearch+"&appid="+ apiKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            .then(function(response) {
+              console.log(response);
+              display.city = response.name;
+              display.temp = response.main.temp;
+              display.humid = response.main.humidity;
+              display.wind = response.wind.speed;
+              //display.uv = response.
+              updateDisplay();
+            });    
+    }
 
 
+
+    function searchDiv(city) {
+        var searchDiv = $("<div>");
+          var p = $("<p>");
+          p.text(city);
+          searchDiv.addClass("search");
+          searchDiv.attr("city",city);
+          searchDiv.append(p);
+          searchDiv.prependTo($(".prevSearch"));
+
+          $(".search").on("click", function() {
+            apiSearch($(this).attr("city"));
+        });
     }
